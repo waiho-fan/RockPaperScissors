9be79a181ce414b440f47242e8bbfe897c22141b
Gary Fan <tomfan419@gmail.com>
2024-12-15 02:13:12 +0800

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Gary on 15/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Gary on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
